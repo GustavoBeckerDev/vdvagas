cca3feabd2c4b9c6bba15cc98e53274fc711a87d
WALDAIR OTTO MENEGASSI <waldair.menegassi@alunos.sc.senac.br>
2025-09-30 22:01:55 -0300

    About Page | Começo

--- app/Views/about/about.php
+++ app/Views/about/about.php
@@ -3,6 +3,32 @@
 
 </section>
 
+<section class="about-us">
+    <div class="container">
+        <div class="about-border">
+            <div class="about-text">
+                <p>Somos uma equipe formada no Senac Videira (SC), a partir do Projeto Integrador, com o objetivo de desenvolver uma plataforma voltada para oportunidades de trabalho.
+
+    Nosso site nasceu da necessidade de criar um espaço que conecte empresas e profissionais, oferecendo praticidade, transparência e eficiência no processo de divulgação e busca por vagas.
+
+    Mais do que cumprir um desafio acadêmico, buscamos aplicar de forma prática os conhecimentos adquiridos em sala de aula, entregando uma solução que gera valor para a comunidade local e regional.
+
+    Acreditamos que iniciativas como esta podem abrir portas, aproximar talentos de empregadores e contribuir para o desenvolvimento profissional de muitas pessoas.</p>
+
+            </div>
+
+        
+        </div>
+    </div>
+
+    <div class="about-border">
+         <div class="container">
+            <div class="about-image">
+                <img src="<?= $baseUrl ?>/public/imagens/capiprise.png" alt="Mascote Capivara Terno">
+            </div>
+        </div>
+    </div>
+</section>
 
 
     <link rel="stylesheet" href="<?= $baseUrl ?>/public/assets/footer.css">
